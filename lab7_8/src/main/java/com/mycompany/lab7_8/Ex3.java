275a3f912b1009aa8d57b0a0567953281cd099c9
ntu-user <ntu-user@dc44be2bcb8e>
2022-10-29 21:50:13 +0000

    Ex3

--- lab7_8/src/main/java/com/mycompany/lab7_8/Ex3.java
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex3.java
@@ -19,16 +19,6 @@ import java.io.IOException;
 public class Ex3 {
 
     public static void main(String[] args) {
-        try {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        //TODO add the code here 
     }
 }
