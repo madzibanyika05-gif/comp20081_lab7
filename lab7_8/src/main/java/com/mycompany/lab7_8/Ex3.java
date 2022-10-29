b68e11775bd8c4da32f9c9ba1c5a2b02f3320447
Machado, Pedro <pedro.machado@ntu.ac.uk>
2022-10-29 22:44:40 +0000

    Update Ex3.java

--- lab7_8/src/main/java/com/mycompany/lab7_8/Ex3.java
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex3.java
@@ -18,17 +18,6 @@ import java.io.IOException;
 public class Ex3 {
 
     public static void main(String[] args) {
-        try {
-            File myObj = new File("Test.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } 
-        catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        //TODO add your code here
     }
 }
