f36af659f8c1b6d0d639d28f0104c2852ccd8cbc
ntu-user <ntu-user@dc44be2bcb8e>
2022-10-29 22:12:25 +0000

    Ex4

---
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab7_8;
+
+/**
+ *
+ * @author ntu-user
+ */
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class Ex4 {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter myWriter = new FileWriter("Test.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
