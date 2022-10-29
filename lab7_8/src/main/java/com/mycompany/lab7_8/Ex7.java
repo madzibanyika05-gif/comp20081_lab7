f146f1529b6394cc7f2bbd3f6e270c0bc39fe9de
ntu-user <ntu-user@dc44be2bcb8e>
2022-10-29 22:33:28 +0000

    Ex7

--- lab7_8/src/main/java/com/mycompany/lab7_8/Ex7.java
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex7.java
@@ -11,12 +11,5 @@ package com.mycompany.lab7_8;
 import java.io.File;  // Import the File class
 
 public class Ex7 {
-  public static void main(String[] args) { 
-    File myObj = new File("Test.txt"); 
-    if (myObj.delete()) { 
-      System.out.println("Deleted the file: " + myObj.getName());
-    } else {
-      System.out.println("Failed to delete the file.");
-    } 
-  } 
+  // TODO add your code here
 }
