c084df0e4d28eec470d54e5c9ee101ffd1772402
Machado, Pedro <pedro.machado@ntu.ac.uk>
2022-10-29 22:43:09 +0000

    Update Ex6.java

--- lab7_8/src/main/java/com/mycompany/lab7_8/Ex6.java
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex6.java
@@ -13,16 +13,6 @@ import java.io.File;  // Import the File class
 public class Ex6 {
 
     public static void main(String[] args) {
-        File myObj = new File("Test.txt");
-        if (myObj.exists()) {
-            System.out.println("File name: " + myObj.getName());
-            System.out.println("Absolute path: " + myObj.getAbsolutePath());
-            System.out.println("Writeable: " + myObj.canWrite());
-            System.out.println("Readable " + myObj.canRead());
-            System.out.println("File size in bytes " + myObj.length());
-        } 
-        else {
-            System.out.println("The file does not exist.");
-        }
+        //TODO add your code here
     }
 }
