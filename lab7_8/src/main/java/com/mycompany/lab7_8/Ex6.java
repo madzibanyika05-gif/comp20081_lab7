ffcd12d6a596c8b43a76d297618c65b0b9d8c001
ntu-user <ntu-user@dc44be2bcb8e>
2022-10-29 22:28:23 +0000

    Ex6

---
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex6.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab7_8;
+
+/**
+ *
+ * @author ntu-user
+ */
+ import java.io.File;  // Import the File class
+
+public class Ex6 { 
+  public static void main(String[] args) {
+    File myObj = new File("Test.txt");
+    if (myObj.exists()) {
+      System.out.println("File name: " + myObj.getName());
+      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+      System.out.println("Writeable: " + myObj.canWrite());
+      System.out.println("Readable " + myObj.canRead());
+      System.out.println("File size in bytes " + myObj.length());
+    } else {
+      System.out.println("The file does not exist.");
+    }
+  }
+}
+
