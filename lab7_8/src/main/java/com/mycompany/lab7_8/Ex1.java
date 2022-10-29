85d36d4447ff07cc4f09ee6b4722fa7bd3f4b4aa
Machado, Pedro <pedro.machado@ntu.ac.uk>
2022-10-29 22:46:58 +0000

    Update Ex1.java

--- lab7_8/src/main/java/com/mycompany/lab7_8/Ex1.java
+++ lab7_8/src/main/java/com/mycompany/lab7_8/Ex1.java
@@ -12,17 +12,10 @@ package com.mycompany.lab7_8;
 public class Ex1 {
 
     public static void main(String args[]) {
-        try {
-            //code that may raise exception
-            System.out.println("Attempting to divide 100 by 0.\n");
-            int data = 100 / 0;
-            System.out.println("This block is not going to run!");
-        } 
-        catch (ArithmeticException e) {
-            System.out.println("Caught the divide by 0 exception!\n" + e);
-        } 
-        finally {
-            System.out.println("This is the finally block");
-        }
+        System.out.println("Attempting to divide 100 by 0.\n");
+        int data = 100 / 0;
+        System.out.println("This block is not going to run!");
+        System.out.println("Caught the divide by 0 exception!\n" + e);
+        System.out.println("This is the finally block");
     }
 }
